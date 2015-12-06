1b806d92efdfd025cc5f3ad060bdbd94bf147286
Adrian Calinescu <foo@snobu.org>
2015-12-06 23:27:00 +0200

    Update busyfish.py

--- podracer/busyfish.py
+++ podracer/busyfish.py
@@ -5,12 +5,12 @@ import sys
 import time
 from termcolor import colored
 
-spinner = itertools.cycle([">))'>    "," >))'>   ","  >))'>  ","   >))'> ","    >))'>","    <'((<","   <'((< ","  <'((<  "," <'((<   ","<'((<    "])
+fish = itertools.cycle([">))'>    "," >))'>   ","  >))'>  ","   >))'> ","    >))'>","    <'((<","   <'((< ","  <'((<  "," <'((<   ","<'((<    "])
 color = 'cyan'
 
 
 while True:
-    sys.stdout.write(colored(spinner.next(), color)) # write the next character
+    sys.stdout.write(colored(fish.next(), color)) # write the next character
     sys.stdout.flush()      # flush stdout buffer (actual character display)
     time.sleep(0.3)
     sys.stdout.write('\r')  # erase the last written char
