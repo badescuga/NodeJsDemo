d998887a4ed0d9cd2de341e4b9cd5742e5bfdfa9
alex badescu <badescuga@gmail.com>
2015-12-07 18:41:08 +0200

    started working with cockpit

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,18 +6,18 @@ start();
 function start() {
 		socket = io.connect();
 		socket.on('connect',(data)=> {
-			
+
 		});
-		
+
 		socket.on('reconnect',(data)=> {
 		var name = $('#currentStatus').text('');
 		if(name && name.length > 0) {
 			login();
 		}
 		});
-		
+
 		socket.on('disconnect',(data)=> {
-		$('#currentStatus').text('Disconnected from server; Trying to reconnect');					
+		$('#currentStatus').text('Disconnected from server; Trying to reconnect');
 		});
 }
 
@@ -37,10 +37,14 @@ function login() {
 
 //function for moving the robot
 function move(direction) {
-	console.log(` >>> web client >>> move ${direction}`)
-	socket.emit('moveRobot', 
+	console.log(` >>> web client >>> move ${direction}`);
+	socket.emit('moveRobot',
 	{ 'direction': direction },
 	(data) => {
-	console.log(` >>> web client >>> response move robot ${data}`);	
+	console.log(` >>> web client >>> response move robot ${data}`);
 	});
+}
+
+function getStream() {
+console.log(' >>> web client >>> get stream');
 }
