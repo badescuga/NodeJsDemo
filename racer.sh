355b7c882cf9eff3a5f4926bc01e7f5ca0b4bca0
snobu <pi@raspberrypi.(none)>
2015-12-09 18:09:11 +0000

    Racer init script and 8bit Sebulba

---
+++ racer.sh
@@ -0,0 +1,16 @@
+echo [RACER] STARTING MJPEG STREAMER.......
+cd /home/pi/NodeJSDemo/mjpeg-streamer
+sudo ./start.sh > /dev/null 2>&1 &
+cd ..
+sleep 2
+echo [RACER] STREAM STARTED
+sudo ps ax | grep mjpg_streamer | grep -v grep
+
+echo "--------------------------------------"
+echo [RACER] INITIALIZING DRIVER: SEBULBA
+cd podracer/hat-invaders
+sudo ./invaders &
+cd ..
+echo "--------------"
+echo [RACER] "RACER READY."
+echo "-------------"
