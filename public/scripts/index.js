8781f6dc37f3df64ce33bcca47ca7c9d2045aed9
alex badescu <badescuga@gmail.com>
2015-12-09 19:50:25 +0200

    added timeout for direction buttons

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,11 +1,17 @@
 
 var socket;
+var directionActive = [];
+var directionTimeout = 1000;
+
 start();
 
 function start() {
+	for (var i = 0; i < 4; i++) {
+		directionActive[0] = false;
+	}
 	socket = io.connect();
 	socket.on('connect', (data) => {
-
+		console.log('connected!');
 	});
 
 	socket.on('reconnect', (data) => {
@@ -22,7 +28,7 @@ function start() {
 	//handling video stream
 
 	socket.on("videoStream", function (data) {
-	 $("#videoStream").attr('src','data:image/jpeg;base64, ' + data);
+		$("#videoStream").attr('src', 'data:image/jpeg;base64, ' + data);
 	});
 
 }
@@ -41,8 +47,35 @@ function login() {
 	});
 };
 
+
 //function for moving the robot
 function move(direction) {
+
+	var dir;
+	switch (direction) {
+		case 'left':
+			dir = 0;
+			break;
+		case 'right':
+			dir = 1;
+			break;
+		case 'up':
+			dir = 2;
+			break;
+		case 'down':
+			dir = 3;
+			break;
+	}
+
+	if (directionActive[dir] === true) {
+		return;
+	} else {
+		directionActive[dir] = true;
+		setTimeout(() => {
+			directionActive[dir] = false;
+		}, directionTimeout);
+	}
+
 	console.log(` >>> web client >>> move ${direction}`);
 	socket.emit('moveRobot',
 		{ 'direction': direction },
@@ -54,13 +87,13 @@ function move(direction) {
 function startStream() {
 
 	socket.emit('startClientVideoStream', null, (data) => {
-	console.log(' >>> web client >>> start video stream');
+		console.log(' >>> web client >>> start video stream');
 	});
 }
 
 function stopStream() {
 	console.log(' >>> web client >>> stop video stream');
 	socket.emit('stopClientVideoStream', null, (data) => {
-	console.log(' >>> web client >>> stop video stream');
+		console.log(' >>> web client >>> stop video stream');
 	});
 }
